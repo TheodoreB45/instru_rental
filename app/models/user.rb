f5338b5b0035b2d1da92551026587d24f10dd737
nicorepetto <nicojrepetto@gmail.com>
2024-11-19 14:29:24 +0000

    created user model using devise

--- app/models/user.rb
+++
@@ -1,5 +0,0 @@
-class User < ApplicationRecord
-  validates :username, uniqueness: true, presence: true
-  validates :password, uniqueness: true, presence: true
-  validates :email, presence: true
-end
