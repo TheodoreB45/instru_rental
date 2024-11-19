66b18add9e0da54e0fa7c515ba105e307624fe5a
Jaime Barros <jabgnecco@gmail.com>
2024-11-19 16:30:20 +0000

    created links of intrument to new page

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  resources :instruments, only: [:index, :destroy, :new, :create]
+  resources :instruments, only: [:index, :destroy, :new, :create, :show]
 end
